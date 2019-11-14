df02531fd923e778409305b284403e0a3b4ff4f0
John Papa <john@johnpapa.net>
2019-11-13 21:04:48 -0500

    sync the trailing comma with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,7 +22,7 @@ module.exports = {
     'prettier/prettier': [
       'error',
       {
-        trailingComma: 'es5',
+        trailingComma: 'all',
         singleQuote: true,
         printWidth: 80,
       },
