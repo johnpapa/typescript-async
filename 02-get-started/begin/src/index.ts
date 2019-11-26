66e695f9e9fb0d0602fc7644756cc24c13b28e9f
John Papa <papa@Johns-MBP.lan>
2019-11-26 18:22:53 -0500

    02 begin

---
+++ 02-get-started/begin/src/index.ts
@@ -0,0 +1,31 @@
+import './design/index.scss';
+import { showMessage } from './lib';
+
+document.querySelector('#bake-cookies').addEventListener('click', async () => {
+  bake();
+});
+
+async function bake() {
+  const title = 'Baking cookies';
+  let counter = 0;
+  console.clear();
+  console.group(title);
+
+  counter++;
+  console.log(`${counter} - Add ingredients`);
+  showMessage(`${counter} - Add ingredients`, title);
+
+  counter++;
+  console.log(`${counter} - Mix ingredients`);
+  showMessage(`${counter} - Mix ingredients`, title, true);
+
+  counter++;
+  console.log(`${counter} - Bake at 325 degrees for 10 minutes`);
+  showMessage(`${counter} - Bake at 325 degrees for 10 minutes`, title, true);
+
+  counter++;
+  console.log(`${counter} - Eat cake`);
+  showMessage(`${counter} - Eat cake`, title, true);
+
+  console.groupEnd();
+}
---
+++ 02-get-started/begin/src/lib/interfaces.ts
@@ -0,0 +1,6 @@
+export interface Hero {
+  id: number;
+  name: string;
+  description: string;
+  email: string;
+}
